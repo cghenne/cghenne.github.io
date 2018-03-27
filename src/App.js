a8488bd979a32737a43d703562a313247e3fa204
Cathleen Ghenne <cathleen@workangel.com>
2018-03-27 11:12:13 -0400

    only show private details is run locally

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
     <div className="container">
       <Header />
       <Hello />
-      <SayHello showPrivateDetails={true} />
+      <SayHello showPrivateDetails={process.env.NODE_ENV !== 'production'} />
       <Experience />
       <Skills />
       <Education />
