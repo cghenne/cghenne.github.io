f4ce81f644e0e37788c9734da290383eb64b0df7
cghenne <cathleen@justbutterit.com>
2019-06-25 15:57:41 -0400

    hooky

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header/Header';
 import Hello from './components/Hello/Hello';
 import SayHello from './components/SayHello/SayHello';
@@ -11,47 +11,33 @@ import Interests from './components/Interests/Interests';
 import References from './components/References/References';
 import Divider from './components/Divider/Divider';
 import PrintDivider from './components/PrintDivider/PrintDivider';
+import './styles/_reset.scss';
 import './App.scss';
 
-type Props = {};
-type State = {
-  isPrintMode: boolean
+const App = () => {
+  const [isPrintMode, setIsPrintMode] = useState(false);
+
+  // this only works in Chrome
+  // but I'm the only one using this features so I've decided it was enough support
+  window.onbeforeprint = () => setIsPrintMode(true);
+  window.onafterprint = () => setIsPrintMode(false);
+
+  return (
+    <div className="container">
+      <Header />
+      <Hello />
+      <SayHello showPrivateDetails={isPrintMode} />
+      <Experience />
+      <Skills />
+      <Education />
+      <Languages />
+      <Interests />
+      <References />
+      <Divider />
+      {isPrintMode && <PrintDivider />}
+    </div>
+  );
 };
-
-class App extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-
-    this.state = {
-      isPrintMode: false
-    };
-
-    // this only works in Chrome
-    // but I'm the only one using this features so I've decided it was enough support
-    window.onbeforeprint = () => this.setState({ isPrintMode: true });
-    window.onafterprint = () => this.setState({ isPrintMode: false });
-  }
-
-  render() {
-    const { isPrintMode } = this.state;
-
-    return (
-      <div className="container">
-        <Header />
-        <Hello />
-        <SayHello showPrivateDetails={isPrintMode} />
-        <Experience />
-        <Skills />
-        <Education />
-        <Languages />
-        <Interests />
-        <References />
-        <Divider />
-        {isPrintMode && <PrintDivider />}
-      </div>
-    );
-  }
-}
 
 App.displayName = 'App';
 
